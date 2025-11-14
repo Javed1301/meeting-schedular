cf983c7da703d596357671bf72ede00491c5b69e
Javed1301 <mdjabedansari13072001@gmail.com>
2025-11-14 13:08:58 +0530

    Made some changes for page-not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,7 +1,31 @@
+"use client";
+
+import { Suspense } from "react";
+
 export default function NotFound() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Page404 />
+    </Suspense>
+  );
+}
+
+function Page404() {
+  // Remove this if not needed:
+  // const params = useSearchParams();
+
   return (
     <div className="text-4xl font-extrabold w-screen pt-75 grid place-items-center">
       <h1>404 - Page Not Found</h1>
     </div>
   );
 }
+
+
+/*
+return (
+    <div className="text-4xl font-extrabold w-screen pt-75 grid place-items-center">
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+*/
--- components/create-event.jsx
+++ components/create-event.jsx
@@ -11,7 +11,7 @@ import {
   Drawer,
   DrawerClose,
   DrawerContent,
-  DrawerDescription,
+  // DrawerDescription,
   DrawerFooter,
   DrawerHeader,
   DrawerTitle,
